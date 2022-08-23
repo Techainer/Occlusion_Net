b0048beeab2c89598c9bdad2f3e80376ee32806c
pdd-99 <gran@techainer.com>
2022-08-23 15:46:42 +0700

    Change build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # DOCKER_BUIDKIT=1 docker build . -t occlusion_net:latest -f aarch64.Dockerfile
-DOCKER_BUIDKIT=0 docker build . -t occlusion_net_mlchain:latest -f aarch64_mlchain.Dockerfile
+DOCKER_BUIDKIT=0 docker build . -t occlusion_net_mlchain:latest -f aarch64.Dockerfile
 # DOCKER_BUIDKIT=1 docker buildx build --build-context occlusion_net:latest=docker-image://occlusion_net:latest@sha256:30b65433e012 . -t occlusion_net_mlchain:latest -f aarch64_mlchain.Dockerfile
